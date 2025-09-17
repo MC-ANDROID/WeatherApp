dfd79fe2c6de52c053b995ea71cc5d22aabd8190
Mykhailo Mykytyn <mykhailo.mykytyn@levi9.com>
2025-09-17 16:04:39 +0300

    gradle updated

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -3,14 +3,37 @@ package com.plcoding.weatherapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldDefaults
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val currentDate =
+                SimpleDateFormat(
+                "dd/MM/yyyy",
+                        Locale.getDefault())
+                    .format(Date())
+
         setContent {
             WeatherAppTheme {
-                
+                Scaffold(contentWindowInsets = ScaffoldDefaults.contentWindowInsets) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    ) {
+                        Text(text = currentDate)
+                    }
+                }
             }
         }
     }
