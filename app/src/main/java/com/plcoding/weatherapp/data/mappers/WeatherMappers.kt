b1a65b3d42e4e0b7192f446f227c927cc3b2b27c
Mykhailo M. <mykhaylo.mykytyn@gmail.com>
2025-09-18 12:44:09 +0300

    added weatherInfo mapper, unit test

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,8 +1,11 @@
 package com.plcoding.weatherapp.data.mappers
 
 import com.plcoding.weatherapp.data.remote.WeatherDataDto
+import com.plcoding.weatherapp.data.remote.WeatherDto
 import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.domain.weather.WeatherType
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
@@ -24,4 +27,17 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
         .mapValues { entry ->
             entry.value.map { it.second }
         }
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find{
+        val hour = if(now.minute < 30) now.hour else now.hour + 1
+        it.time.hour == hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
 }
